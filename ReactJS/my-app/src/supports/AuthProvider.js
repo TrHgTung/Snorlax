e7ceafe8fa79ff59c90d343195c5c40ff18177cf
TrHgTung <89836403+TrHgTung@users.noreply.github.com>
2024-06-24 17:11:16 +0700

    save login session with setAuth

--- ReactJS/my-app/src/supports/AuthProvider.js
+++ ReactJS/my-app/src/supports/AuthProvider.js
@@ -8,13 +8,20 @@ export const useAuth = () => {
 };
 
 export const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState({ token: null });
+    const [auth, setAuth] = useState({
+        token : localStorage.getItem('token') || null,
+        isAuthenticated : localStorage.getItem('token') ? true : false
+    });
+
     const navigate = useNavigate();
 
     useEffect(() => {
         const token = localStorage.getItem('token');
         if (localStorage.getItem('token')) {
-            setAuth({ token });
+            setAuth({ 
+                token: token, 
+                isAuthenticated: true 
+            });
         } else {
             navigate('/login');
         }
@@ -22,12 +29,18 @@ export const AuthProvider = ({ children }) => {
 
     const login = (token) => {
         localStorage.setItem('token', token);
-        setAuth({ token });
+        setAuth({ 
+            token: token, 
+            isAuthenticated: true 
+        });
     };
 
     const logout = () => {
         localStorage.removeItem('token');
-        setAuth({ token: null });
+        setAuth({ 
+            token: null,
+            isAuthenticated: false 
+        });
         navigate('/login');
     };
 
